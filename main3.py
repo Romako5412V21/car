2dbb83c2290827038406d1b1a659101d176c4caa
Romako5412V21 <167633505+Romako5412V21@users.noreply.github.com>
2024-05-18 19:21:27 +0300

    Add files via upload

--- main3.py
+++ main3.py
@@ -1,6 +1,8 @@
 import pygame 
 pygame.init()
 y = 1
+p = 1
+
 
 class Player():
     def __init__(self,x,y,width,height,image):
@@ -10,9 +12,9 @@ class Player():
         self.rect.x = x # координати по ширині
         self.rect.y = y # координати по висоті
         self.width = width # ширина
-        self.height = height # висота
+        self.height = height # висотаaaaaaa 
         self.is_jump = False  # змінна для визначення стану прижку
-        self.jump_count = 10  # лічильник для керування прижком
+        self.jump_count = 10   # лічильник для керування прижком
     def jumping(self):  # функція для виконання прижку
         if not self.is_jump:  # якщо персонаж не в стані прижку
             self.is_jump = True  # зміна стану прижку
@@ -29,17 +31,18 @@ class Player():
                 neg = 1  # коефіцієнт для керування напрямком прижку
                 if self.jump_count < 0:  # якщо лічильник менше 0
                     neg = -1  # змінити напрямок прижку
-                self.rect.y -= (self.jump_count ** 2) * 0.5 * neg  # формула для прижку
+                self.rect.y -= (self.jump_count ** 2) * 0.4 * neg  # формула для прижку
                 self.jump_count -= 1  # зменшення лічильника
             else:  # якщо лічильник вийшов за межі -10
                 self.is_jump = False  # зміна стану прижку
+
+    def move(self):
+        self.rect.x -= p
+        if self.rect.x <= 0:
+            self.rect.x = 1366
     
      
 
-    def move(self):
-        keys = pygame.key.get_pressed() # зберігаємо всі можливі натиснуті клавіші в список keys
-        if keys[pygame.K_a]: # якщо натиснута клавіша "стрілка ліворуч"
-            self.rect.x -= 2  # змінюємо координати гравця по x на -2
     
     
 
@@ -53,20 +56,18 @@ background_image = pygame.transform.scale(background_image, (1366,700)) # зад
 
 # Колір фону
 bg_color = (255, 255, 255)
-player = Player(100, 450, 50, 50, 'Dino.png')
+player = Player(100, 650, 50, 50, 'Dino.png')
 kak = Player(633, 430, 100, 100, 'kak.png')
-kak1 = Player(500, 430, 100, 100, 'kak.png')
-kak2 = Player(500, 430, 100, 100, 'kak.png')
-kak3 = Player(500, 430, 100, 100, 'kak.png')
-kak4 = Player(500, 430, 100, 100, 'kak.png')
-
-kaks = [kak,kak1,kak2,kak3,kak4]
+kak1 = Player(433, 430, 100, 100, 'kak.png')
+kak2 = Player(833, 430, 100, 100, 'kak.png')
+kak3 = Player(1033, 430, 100, 100, 'KAKTES.png')
+kak4 = Player(1233, 430, 100, 100, 'kak.png')
 
 # Головний цикл гри
 clock = pygame.time.Clock()
 game = True
 while game:
-    
+    p += 0.000001
 
        # Відображення фону
     window.blit(background_image, (0, 0))
@@ -89,14 +90,25 @@ while game:
     window.blit(kak1.image, (kak1.rect.x, kak1.rect.y))  
     window.blit(kak2.image, (kak2.rect.x, kak2.rect.y)) 
     window.blit(kak3.image, (kak3.rect.x, kak3.rect.y)) 
-    window.blit(kak4.image, (kak4.rect.x, kak4.rect.y)) 
-
+    window.blit(kak4.image, (kak4.rect.x, kak4.rect.y))  
     
-    if kak.rect.colliderect(player.rect):
+    if player.rect.colliderect(kak.rect):
+       print('Зіткнення відбулося')
+    if player.rect.colliderect(kak1.rect):
+       print('Зіткнення відбулося')
+    if player.rect.colliderect(kak2.rect):
+       print('Зіткнення відбулося')
+    if player.rect.colliderect(kak3.rect):
+       print('Зіткнення відбулося')
+    if player.rect.colliderect(kak4.rect):
        print('Зіткнення відбулося')
        game = False
 
-    kaks.move()
+    kak.move()
+    kak1.move()
+    kak2.move()
+    kak3.move()
+    kak4.move()
 
     clock.tick(60)
     pygame.display.update()
