a88def13c56a06d8321fefd111d40405799ec06a
Romako5412V21 <167633505+Romako5412V21@users.noreply.github.com>
2024-05-25 19:27:02 +0300

    Add files via upload

--- main3.py
+++ main3.py
@@ -1,7 +1,8 @@
 import pygame 
 pygame.init()
 y = 1
-p = 1
+p = 7
+f = 0
 
 
 class Player():
@@ -14,11 +15,11 @@ class Player():
         self.width = width # ширина
         self.height = height # висотаaaaaaa 
         self.is_jump = False  # змінна для визначення стану прижку
-        self.jump_count = 10   # лічильник для керування прижком
+        self.jump_count = 13   # лічильник для керування прижком
     def jumping(self):  # функція для виконання прижку
         if not self.is_jump:  # якщо персонаж не в стані прижку
             self.is_jump = True  # зміна стану прижку
-            self.jump_count = 10  # початкове значення лічильника прижку
+            self.jump_count = 13  # початкове значення лічильника прижку
 
 
     def jump(self):  # метод для руху персонажа
@@ -27,12 +28,12 @@ class Player():
             self.jumping()  # виконати прижок
 
         if self.is_jump:  # якщо персонаж у стані прижку
-            if self.jump_count >= -10:  # поки лічильник більше або рівний -10
+            if self.jump_count >= -13:  # поки лічильник більше або рівний -10
                 neg = 1  # коефіцієнт для керування напрямком прижку
                 if self.jump_count < 0:  # якщо лічильник менше 0
                     neg = -1  # змінити напрямок прижку
-                self.rect.y -= (self.jump_count ** 2) * 0.4 * neg  # формула для прижку
-                self.jump_count -= 1  # зменшення лічильника
+                self.rect.y -= (self.jump_count ** 2) * 0.2 * neg  # формула для прижку
+                self.jump_count -= 0.5  # зменшення лічильника
             else:  # якщо лічильник вийшов за межі -10
                 self.is_jump = False  # зміна стану прижку
 
@@ -56,18 +57,25 @@ background_image = pygame.transform.scale(background_image, (1366,700)) # зад
 
 # Колір фону
 bg_color = (255, 255, 255)
-player = Player(100, 650, 50, 50, 'Dino.png')
+player = Player(100, 480, 50, 50, 'Dino.png')
 kak = Player(633, 430, 100, 100, 'kak.png')
-kak1 = Player(433, 430, 100, 100, 'kak.png')
-kak2 = Player(833, 430, 100, 100, 'kak.png')
-kak3 = Player(1033, 430, 100, 100, 'KAKTES.png')
+kak1 = Player(633, 430, 100, 100, 'kak.png')
+kak2 = Player(1033, 430, 100, 100, 'kak.png')
+kak3 = Player(1433, 430, 100, 100, 'KAKTES.png')
 kak4 = Player(1233, 430, 100, 100, 'kak.png')
+kak5 = Player(1433, 430, 100, 100, 'kak.png')
+kak6 = Player(1633, 430, 100, 100, 'KAKTES.png')
 
 # Головний цикл гри
 clock = pygame.time.Clock()
+
+
+font1 = pygame.font.Font(None, 36) # 36 - це розмір тексту, при потребі можна змінити на бажаний розмір
+text = font1.render(str(f), True, (0,0,0)) # "Привіт!" - текст, який виводиться; (0,0,0) - колір у форматі RGB
 game = True
 while game:
-    p += 0.000001
+    p += 0.001
+    f += 1 
 
        # Відображення фону
     window.blit(background_image, (0, 0))
@@ -76,6 +84,8 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+    text = font1.render(str(f), True, (0,0,0))
+    window.blit(text, (100,100)) # (10,10) - координати розміщення тексту
 
 
     # виклик функції прижку до персонажу
@@ -91,24 +101,38 @@ while game:
     window.blit(kak2.image, (kak2.rect.x, kak2.rect.y)) 
     window.blit(kak3.image, (kak3.rect.x, kak3.rect.y)) 
     window.blit(kak4.image, (kak4.rect.x, kak4.rect.y))  
+    window.blit(kak5.image, (kak5.rect.x, kak5.rect.y)) 
+    window.blit(kak6.image, (kak6.rect.x, kak6.rect.y)) 
     
     if player.rect.colliderect(kak.rect):
        print('Зіткнення відбулося')
+       game = False
     if player.rect.colliderect(kak1.rect):
        print('Зіткнення відбулося')
+       game = False
     if player.rect.colliderect(kak2.rect):
        print('Зіткнення відбулося')
+       game = False
     if player.rect.colliderect(kak3.rect):
        print('Зіткнення відбулося')
+       game = False
     if player.rect.colliderect(kak4.rect):
+       print('Зіткнення відбулося')
+       game = False
+    if player.rect.colliderect(kak5.rect):
+       print('Зіткнення відбулося')
+       game = False
+    if player.rect.colliderect(kak6.rect):
        print('Зіткнення відбулося')
        game = False
 
     kak.move()
     kak1.move()
     kak2.move()
-    kak3.move()
-    kak4.move()
+    # kak3.move()
+    # kak4.move()
+    # kak5.move()
+    # kak6.move()
 
     clock.tick(60)
     pygame.display.update()
